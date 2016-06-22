095fef5fea97036d31318ff5492604c46f24517a
caodanping <hfut_cao@163.com>
2016-06-22 21:41:28 +0800

    init commit

---
+++ DataAnalyzer/MainForm.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace DataAnalyzer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.inFileTextBox = new System.Windows.Forms.TextBox();
+            this.selectButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.importDataWorker = new System.ComponentModel.BackgroundWorker();
+            this.labelMessage = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.outFileSelectButton = new System.Windows.Forms.Button();
+            this.outFileTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "输入数据文件:";
+            // 
+            // inFileTextBox
+            // 
+            this.inFileTextBox.Location = new System.Drawing.Point(120, 23);
+            this.inFileTextBox.Name = "inFileTextBox";
+            this.inFileTextBox.Size = new System.Drawing.Size(264, 21);
+            this.inFileTextBox.TabIndex = 1;
+            // 
+            // selectButton
+            // 
+            this.selectButton.Location = new System.Drawing.Point(390, 21);
+            this.selectButton.Name = "selectButton";
+            this.selectButton.Size = new System.Drawing.Size(49, 23);
+            this.selectButton.TabIndex = 2;
+            this.selectButton.Text = "选择";
+            this.selectButton.UseVisualStyleBackColor = true;
+            this.selectButton.Click += new System.EventHandler(this.selectButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(302, 87);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(137, 41);
+            this.startButton.TabIndex = 5;
+            this.startButton.Text = "开始统计";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(27, 87);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(269, 23);
+            this.progressBar.TabIndex = 6;
+            this.progressBar.Visible = false;
+            // 
+            // importDataWorker
+            // 
+            this.importDataWorker.WorkerReportsProgress = true;
+            this.importDataWorker.WorkerSupportsCancellation = true;
+            this.importDataWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.importDataWorker_DoWork);
+            this.importDataWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.importDataWorker_ProgressChanged);
+            this.importDataWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.importDataWorker_RunWorkerCompleted);
+            // 
+            // labelMessage
+            // 
+            this.labelMessage.AutoSize = true;
+            this.labelMessage.Location = new System.Drawing.Point(25, 116);
+            this.labelMessage.Name = "labelMessage";
+            this.labelMessage.Size = new System.Drawing.Size(0, 12);
+            this.labelMessage.TabIndex = 7;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "结果文件:";
+            // 
+            // outFileSelectButton
+            // 
+            this.outFileSelectButton.Location = new System.Drawing.Point(390, 50);
+            this.outFileSelectButton.Name = "outFileSelectButton";
+            this.outFileSelectButton.Size = new System.Drawing.Size(49, 23);
+            this.outFileSelectButton.TabIndex = 10;
+            this.outFileSelectButton.Text = "选择";
+            this.outFileSelectButton.UseVisualStyleBackColor = true;
+            this.outFileSelectButton.Click += new System.EventHandler(this.outFileSelectButton_Click);
+            // 
+            // outFileTextBox
+            // 
+            this.outFileTextBox.Location = new System.Drawing.Point(120, 52);
+            this.outFileTextBox.Name = "outFileTextBox";
+            this.outFileTextBox.Size = new System.Drawing.Size(264, 21);
+            this.outFileTextBox.TabIndex = 9;
+            this.outFileTextBox.Text = "D:\\result.csv";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 151);
+            this.Controls.Add(this.outFileSelectButton);
+            this.Controls.Add(this.outFileTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelMessage);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.selectButton);
+            this.Controls.Add(this.inFileTextBox);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "数据分析";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inFileTextBox;
+        private System.Windows.Forms.Button selectButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.ComponentModel.BackgroundWorker importDataWorker;
+        private System.Windows.Forms.Label labelMessage;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button outFileSelectButton;
+        private System.Windows.Forms.TextBox outFileTextBox;
+    }
+}
+
---
+++ DataAnalyzer/MainForm.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Data.SQLite;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataAnalyzer
+{
+    public partial class MainForm : Form
+    {
+        enum State
+        {
+            Running, Stopped, Canceling
+        }
+
+        private String inputFileName;
+        private String saveFileName;
+        private int progress = 0;
+        private long readBytes = 0;
+        private int importedRecords = 0;
+        private long totalBytes = 0;
+        private DateTime startTime;
+        private bool importCompleted = false;
+        private int writeCount = 0;
+        private int totalCount = 0;
+
+        private State state = State.Stopped;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (state == State.Stopped)
+            {
+                inputFileName = inFileTextBox.Text;
+                if (String.IsNullOrEmpty(inputFileName))
+                {
+                    MessageBox.Show("必须要指定输入数据文件！");
+                    return;
+                }
+
+                saveFileName = outFileTextBox.Text;
+                if (String.IsNullOrEmpty(saveFileName))
+                {
+                    MessageBox.Show("必须要指定数据文件！");
+                    return;
+                }
+                ResultState();
+                startTime = DateTime.Now;
+                state = State.Running;
+
+                importDataWorker.RunWorkerAsync();
+                timer.Start();
+
+                startButton.Text = "停止统计";
+                progressBar.Visible = true;
+            }
+            else if(state == State.Running)
+            {
+                importDataWorker.CancelAsync();
+            }
+        }
+
+        private void selectButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "csv文件(逗号或Tab分隔)|*.csv|文本文件(逗号或Tab分隔)|*.txt";
+            dialog.Multiselect = false;
+            if (DialogResult.OK == dialog.ShowDialog())
+            {
+                inFileTextBox.Text = dialog.FileName;
+            }
+            else
+            {
+                inFileTextBox.Text = String.Empty;
+            }
+        }
+
+        private void importDataWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            using (StreamReader reader = File.OpenText(inputFileName))
+            {
+                DbProviderFactory factory = DbProviderFactories.GetFactory("System.Data.SQLite");
+                using (DbConnection conn = factory.CreateConnection())
+                {
+                    conn.ConnectionString = "Data Source=data.db";
+                    conn.Open();
+                    ClearData(conn);
+                    DbCommand cmd = conn.CreateCommand();
+                    cmd.CommandText = "INSERT INTO data(text) VALUES(?)";
+                    totalBytes = reader.BaseStream.Length;
+                    DbTransaction tx = conn.BeginTransaction();
+                    for (String line = reader.ReadLine(); line != null; line = reader.ReadLine())
+                    {
+                        if (importDataWorker.CancellationPending)
+                        {
+                            e.Cancel = true;
+                            break;
+                        }
+                        readBytes += (line.Length + 1);
+                        if (String.IsNullOrWhiteSpace(line)) continue;
+                        String[] cols = line.Split(',', ' ', '\t');
+                        foreach (String col in cols)
+                        {
+                            string val = col.Trim();
+
+                            if (String.IsNullOrEmpty(val)) continue;
+
+                            cmd.Parameters.Clear();
+                            DbParameter param = cmd.CreateParameter();
+                            param.DbType = DbType.String;
+                            param.Value = val;
+                            cmd.Parameters.Add(param);
+                            cmd.ExecuteNonQuery();
+                            importedRecords++;
+                        }
+                        int prog = (int)(readBytes * 100 / totalBytes);
+                        if (prog != progress)
+                        {
+                            progress = prog;
+                            importDataWorker.ReportProgress(prog);
+                        }
+                    }
+                    tx.Commit();
+
+                    progress = 0;
+                    totalCount = importedRecords;
+                    importCompleted = true;
+                    importDataWorker.ReportProgress(progress);
+
+                    startTime = DateTime.Now;
+
+                    using (StreamWriter writer = new StreamWriter(File.Open(saveFileName, FileMode.OpenOrCreate)))
+                    {
+                        DbCommand query = conn.CreateCommand();
+                        query.CommandText = "SELECT text, count(text) AS cnt FROM data GROUP BY text";
+                        using (DbDataReader dbReader = query.ExecuteReader())
+                        {
+                            while (dbReader.Read()) 
+                            {
+                                if (importDataWorker.CancellationPending)
+                                {
+                                    e.Cancel = true;
+                                    break;
+                                }
+                                writer.WriteLine(dbReader.GetString(0) + "," + dbReader.GetInt32(1));
+                                writeCount++;
+
+                                int prog = (int)(writeCount * 100 / totalCount);
+                                if (prog != progress)
+                                {
+                                    progress = prog;
+                                    importDataWorker.ReportProgress(prog);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            
+        }
+
+        private static void ClearData(DbConnection conn)
+        {
+            DbTransaction tx = conn.BeginTransaction();
+            DbCommand clearCmd = conn.CreateCommand();
+            clearCmd.CommandText = "DELETE FROM data";
+            clearCmd.ExecuteNonQuery();
+            tx.Commit();
+        }
+
+        private void importDataWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Maximum = 100;
+            progressBar.Value = progress;
+        }
+
+        private void importDataWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            timer.Stop();
+
+            Ready();
+
+            if (e.Error != null)
+            {
+                MessageBox.Show("处理发生错误！");
+            }
+            else if (e.Cancelled)
+            {
+                labelMessage.Text = "操作已取消.";
+            }
+            else
+            {
+                labelMessage.Text = "处理完成.";
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (!importCompleted)
+            {
+                TimeSpan cost = DateTime.Now - startTime;
+                long ticks = (long)((double)cost.Ticks * ((double)totalBytes / readBytes));
+                TimeSpan total = new TimeSpan(ticks);
+                TimeSpan left = total.Subtract(cost);
+                labelMessage.Text = "已导入:" + importedRecords + ".   预估剩余时间:" + ToTimeSpanString(ref left);
+            }
+            else
+            {
+                TimeSpan cost = DateTime.Now - startTime;
+                long ticks = (long)((double)cost.Ticks * ((double)totalBytes / readBytes));
+                TimeSpan total = new TimeSpan(ticks);
+                TimeSpan left = total.Subtract(cost);
+                labelMessage.Text = "已写入:" + writeCount + "/" + totalCount + ".   预估剩余时间:" + ToTimeSpanString(ref left);
+            }
+        }
+
+        private string ToTimeSpanString(ref TimeSpan timespan)
+        {
+            String txt = "";
+            if (timespan.Hours > 0)
+            {
+                txt = timespan.Hours + "时:" + timespan.Minutes + "分";
+            }
+            else if (timespan.Minutes > 0)
+            {
+                txt = timespan.Minutes + "分:" + timespan.Seconds + "秒";
+            }
+            else
+            {
+                txt = timespan.Seconds + "秒";
+            }
+            return txt;
+        }
+
+        private void Ready()
+        {
+            state = State.Stopped;
+            startButton.Text = "开始统计";
+            progressBar.Visible = false;
+        }
+
+        private void ResultState()
+        {
+            progress = 0;
+            progressBar.Value = 0;
+            readBytes = 0;
+            importedRecords = 0;
+            totalBytes = 0;
+            importCompleted = false;
+            writeCount = 0;
+            totalCount = 0;
+            state = State.Stopped;
+        }
+
+
+        private void outFileSelectButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "csv文件(逗号分隔)|*.csv|文本文件(逗号分隔)|*.txt";
+            if (DialogResult.OK == dlg.ShowDialog())
+            {
+                outFileTextBox.Text = dlg.FileName;
+            }
+        }
+    }
+}
